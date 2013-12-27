c5f164048ced35c49f5dc220dae6f552b9206cab
Serin Donnelly <serin.donnelly@gmail.com>
2013-12-27 23:22:24 +0000

    static functions are static

--- common.h
+++ common.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -6,20 +8,21 @@
 //#include "AnalysisStack.h"
 //static AnalysisStack* analysisStack;
 
-void parseFile(picojson::value& out, const std::string& filename)
+static void parseFile(picojson::value& out, const std::string& filename)
 {
 	std::ifstream f(filename);
 	picojson::parse(out, f);
 }
 
-picojson::value loadJSON(const std::string& filename)
+static picojson::value loadJSON(const std::string& filename)
 {
 	picojson::value v;
 	parseFile(v, filename);
 	return v; // inefficient copy; use parseFile if this is used in a bottleneck
+	// check that this one actually works
 }
 
-void saveJSON(const std::string& filename, const picojson::value& document)
+static void saveJSON(const std::string& filename, const picojson::value& document)
 {
 	std::ofstream f(filename);
 	f << document;
