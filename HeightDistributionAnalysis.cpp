886b9e570d7cfc5c912c7170464e169e319e066d
Serin Donnelly <serin.donnelly@gmail.com>
2014-01-01 22:14:55 +0000

    height dependence

--- HeightDistributionAnalysis.cpp
+++ HeightDistributionAnalysis.cpp
@@ -1,4 +1,6 @@
 #include "HeightDistributionAnalysis.h"
+#include "ForestAnalysis.h"
+#include "AxesAnalysis.h"
 
 
 HeightDistributionAnalysis::HeightDistributionAnalysis()
@@ -8,4 +10,48 @@ HeightDistributionAnalysis::HeightDistributionAnalysis()
 
 HeightDistributionAnalysis::~HeightDistributionAnalysis()
 {
+}
+/***********************************************************************
+ *  Method: HeightDistributionAnalysis::selectDistribution
+ *  Params: const Node *n, int &selection
+ * Returns: bool
+ * Effects: 
+ ***********************************************************************/
+bool
+HeightDistributionAnalysis::selectDistribution(const Node *n, int &selection) const
+{
+	return false;
+	//height dependent distribution is unworkable
+}
+
+
+/***********************************************************************
+ *  Method: HeightDistributionAnalysis::collectSample
+ *  Params: std::vector<float> &samplesX, std::vector<float> &samplesY, std::vector<float> &sampleWeights
+ * Returns: void
+ * Effects: 
+ ***********************************************************************/
+void
+HeightDistributionAnalysis::collectSample(
+std::vector<float> &samplesX,
+std::vector<float> &samplesY,
+std::vector<float> &sampleWeights) const
+{
+	const Forest* f = ((ForestAnalysis*)inputs[0])->getForest();
+	const vecN pf = ((AxesAnalysis*)inputs[1])->getPF();
+	std::vector<Point> samples;
+	std::vector<int> sampleIDs;
+	std::vector<float> sampleRatios;
+	f->samplePoints(samples, 500000, &sampleIDs, &sampleRatios); // TODO factor out magic number of samples
+	for (int i = 0; i < samples.size() && i < sampleIDs.size() && sampleRatios.size(); i++)
+	{
+		float angle = pf.cosine(f->getSegment(sampleIDs[i])->getVector());
+		float height = pf.dot(samples[i]);
+		samplesX.push_back(angle);
+		samplesY.push_back(height);
+		sampleWeights.push_back(1.f-sampleRatios[i]);
+		// TODO collect point distance along segment.
+		// needs alteration to Forest or comparison between
+		// norm(sample-proximal node) and segment length.
+	}
 }
--- HeightDistributionAnalysis.h
+++ HeightDistributionAnalysis.h
@@ -6,5 +6,11 @@ class HeightDistributionAnalysis :
 public:
 	HeightDistributionAnalysis();
 	~HeightDistributionAnalysis();
+private:
+	virtual bool selectDistribution(const Node* n, int& selection) const;
+	virtual void collectSample(
+		std::vector<float>& samplesX,
+		std::vector<float>& samplesY,
+		std::vector<float>& sampleWeights) const;
 };
 
