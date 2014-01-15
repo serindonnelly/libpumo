6d2a777bab6840032a8e188c471b914717dc9795
Serin Donnelly <serin.donnelly@gmail.com>
2014-01-15 17:11:31 +0000

    note start time

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "teestream.h"
 #include "LogStream.h"
 #include "common.h"
+#include "TCLForest.h"
 
 int main(int argc, char** argv)
 {
@@ -15,9 +16,9 @@ int main(int argc, char** argv)
 	logging::lerr  = new CLogger("ERROR  ", tlog);
 	logging::lwarn = new CLogger("WARNING", tlog);
 	logging::linfo = new CLogger("INFO   ", tlog);
-	std::cout << "Beginning log" << std::endl;
+	std::cout << currentTimeString() << " Beginning log" << std::endl;
 	//*logging::linfo << "Testing" << std::endl;
 	//AnalysisStack* analysisStack = new AnalysisStack(argv[1]);
-	AnalysisStack* analysisStack = new AnalysisStack("analysis_stack.json");
+	AnalysisStack* analysisStack = new AnalysisStack("analysis_stack_all.json");
 	return 0;
 }
